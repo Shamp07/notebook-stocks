dffa423c9e37fd0533e42eda9dbcf044337f9a5b
Shamp07 <blog.shamp@gmail.com>
2025-07-02 10:51:44 +0900

    feat: add stylistic and stricter eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -11,9 +12,19 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      {
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+        },
+      },
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      stylistic.configs.recommended
     ],
     languageOptions: {
       ecmaVersion: 2020,
